5348470e6f50de50f613e339c9d79a2bf71ee5d6
Rajasekar M <rajasekar.1999@gmail.com>
2024-10-16 00:20:13 +0530

    Deleted unwanted stuff

--- force-app/main/default/lwc/iFrameForReactJS/iFrameForReactJS.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import myReactApp from '@salesforce/resourceUrl/mockReactJs';
-
-export default class IFrameForReactJS extends LightningElement {
-    reactAppUrl = `${myReactApp}/index.html`;
-}
