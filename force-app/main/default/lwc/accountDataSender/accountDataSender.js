76c6081d82ca15a2da609a0efa022e3dc6a14ece
Rajasekar M <rajasekar.1999@gmail.com>
2024-10-15 23:55:25 +0530

    Some Meaningful Change

--- force-app/main/default/lwc/accountDataSender/accountDataSender.js
+++ force-app/main/default/lwc/accountDataSender/accountDataSender.js
@@ -1,8 +1,6 @@
-// lwc/accountDataSender/accountDataSender.js
-import { LightningElement, api } from 'lwc';
+import { LightningElement } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import getAccountDataForReactApp from '@salesforce/apex/AccountDataController.getAccountDataForReactApp';
 
 export default class AccountDataSender extends LightningElement {
     recordId;
@@ -14,19 +12,8 @@ export default class AccountDataSender extends LightningElement {
 
     async sendDataToReactApp() {
         try {
-            const accountData = await getAccountDataForReactApp({ accountId: this.recordId });
-            const reactAppUrl = 'https://mock-reactjs.vercel.app/';
-            
-            // Open the React app in a new window
-            const reactWindow = window.open(reactAppUrl, '_blank');
-            
-            // Wait for the React app to load
-            reactWindow.onload = () => {
-                // Post the data to the React app
-                reactWindow.postMessage({ type: 'SALESFORCE_DATA', payload: accountData }, '*');
-            };
-
-            this.showToast('Success', 'React app opened with account data', 'success');
+            const reactAppUrl = 'https://mock-reactjs.vercel.app/?accountId='+this.recordId;
+            window.open(reactAppUrl, '_blank');
         } catch (error) {
             console.error('Error sending data to React app:', error);
             this.showToast('Error', error.message || 'Failed to send data to React app', 'error');
@@ -49,35 +36,6 @@ export default class AccountDataSender extends LightningElement {
         const recordId = urlParams.get('recordId');
         console.log('Extracted Record ID:', recordId); // For debugging
         return recordId;
-    }
-
-    async sendDataToReactApp() {
-        try {
-            const accountData = await getAccountDataForReactApp({ accountId: this.recordId });
-            console.log('Account data received:', accountData);
-
-            const reactAppUrl = 'https://mock-reactjs.vercel.app/';
-            console.log('Opening React app:', reactAppUrl);
-            
-            const reactWindow = window.open(reactAppUrl, '_blank');
-            
-            if (!reactWindow) {
-                throw new Error('Unable to open React app window. Please check your pop-up blocker settings.');
-            }
-
-            reactWindow.onload = () => {
-                console.log('React app window loaded');
-                reactWindow.postMessage({ type: 'SALESFORCE_DATA', payload: accountData }, '*');
-                console.log('Data posted to React app');
-            };
-
-            this.showToast('Success', 'React app opened with account data', 'success');
-        } catch (error) {
-            console.error('Error sending data to React app:', error);
-            this.showToast('Error', error.message || 'Failed to send data to React app', 'error');
-        } finally {
-            this.dispatchEvent(new CloseActionScreenEvent());
-        }
     }
 
     showToast(title, message, variant) {
